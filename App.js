ce8b38435181deec7cf2f4ce2669c6a7f1474634
sophiecode1105 <sophiecode1105@gmail.com>
2021-12-16 00:47:41 +0900

    weather app css

--- App.js
+++ App.js
@@ -4,13 +4,29 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return ( //레이아웃만들기 //부모컴포넌트 기준이기에 매우 중요
-    <View style={{ flex : 1 }}> 
-      <View style = {{ flex: 1, backgroundColor: "tomato"}}></View>
-      <View style = {{ flex: 1.5, backgroundColor: "teal"}}></View>
-      <View style = {{ flex: 1, backgroundColor: "orange"}}></View> 
+    <View style={styles.container}> 
+     <View style={styles.city}>
+     <Text>Seoul</Text>
+     </View>
+     <View style={styles.weather}></View>
     </View>
   );
 }
+  const styles = StyleSheet.create({
+    container : {
+      flex: 1,
+      backgroundColor : 'pink'
+    },
+    city: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    weather: {
+      flex:2,
+      backgroundColor : 'powderblue'
+    }
+  })
 //view = flexbox
 //웹이서는 display : flex; 쓰고 felx-direction
 //웹에서는 Row
