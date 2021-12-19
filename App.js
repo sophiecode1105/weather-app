8cea2c15005c3f6aaf75ba27e366a30be9e3b999
sophiecode1105 <sophiecode1105@gmail.com>
2021-12-19 20:35:22 +0900

    style part2

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView,Dimensions } from 'react-native';
+
+const {width : SCREEN_WIDTH} = Dimensions.get('window');
+
+console.log(SCREEN_WIDTH)
+
 
 export default function App() {
   return ( //레이아웃만들기 //부모컴포넌트 기준이기에 매우 중요
@@ -8,12 +13,24 @@ export default function App() {
      <View style={styles.city}>
      <Text style={styles.cityName}>Seoul</Text>
      </View>
-     <View style={styles.weather}>
+     <ScrollView horizontal contentContainerstyle={styles.weather}> 
        <View style={styles.day}>
-       <Text style = {styles.temp}>27</Text>
-       <Text style = {styles.description}>Sunny</Text>
+        <Text style = {styles.temp}>27</Text>
+        <Text style = {styles.description}>Sunny</Text>
        </View>
-     </View>
+       <View style={styles.day}>
+        <Text style = {styles.temp}>27</Text>
+        <Text style = {styles.description}>Sunny</Text>
+       </View>
+       <View style={styles.day}>
+        <Text style = {styles.temp}>27</Text>
+        <Text style = {styles.description}>Sunny</Text>
+       </View>
+       <View style={styles.day}>
+        <Text style = {styles.temp}>27</Text>
+        <Text style = {styles.description}>Sunny</Text>
+       </View>
+     </ScrollView>
     </View>
   );
 }
@@ -32,12 +49,13 @@ export default function App() {
       fontWeight : '500'
     },
     weather: {
-      flex:3,
-      // backgroundColor : 'powderblue'
+      // flex:3, 없애줘야한다. 이유 : 스크롤뷰가 스크린보다 커져야지 효과가 자연스럽게 보여짐
+      backgroundColor : 'blue'
     },
     day:{
-      flex:1,
+      // flex:1,
       // justifyContent : 'center',
+      width:  SCREEN_WIDTH,
       alignItems : 'center',
       backgroundColor : 'powderblue'
     },
@@ -54,5 +72,6 @@ export default function App() {
 //웹이서는 display : flex; 쓰고 felx-direction
 //웹에서는 Row
 //모바일 column이 기본값
+//scrollview. 사이즈가 조금 이상해짐을 관찰할 수있다.style이 아닌 contentstyle
 
 
