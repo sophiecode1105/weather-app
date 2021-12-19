baf6dd1bd85fa787130f727412a87e96daf4c781
sophiecode1105 <sophiecode1105@gmail.com>
2021-12-19 20:10:18 +0900

    style practice

--- App.js
+++ App.js
@@ -6,9 +6,14 @@ export default function App() {
   return ( //레이아웃만들기 //부모컴포넌트 기준이기에 매우 중요
     <View style={styles.container}> 
      <View style={styles.city}>
-     <Text>Seoul</Text>
+     <Text style={styles.cityName}>Seoul</Text>
      </View>
-     <View style={styles.weather}></View>
+     <View style={styles.weather}>
+       <View style={styles.day}>
+       <Text style = {styles.temp}>27</Text>
+       <Text style = {styles.description}>Sunny</Text>
+       </View>
+     </View>
     </View>
   );
 }
@@ -18,13 +23,31 @@ export default function App() {
       backgroundColor : 'pink'
     },
     city: {
-      flex: 1,
+      flex: 1.2,
       justifyContent: 'center',
       alignItems: 'center'
     },
+    cityName:{
+      fontSize : 68,
+      fontWeight : '500'
+    },
     weather: {
-      flex:2,
+      flex:3,
+      // backgroundColor : 'powderblue'
+    },
+    day:{
+      flex:1,
+      // justifyContent : 'center',
+      alignItems : 'center',
       backgroundColor : 'powderblue'
+    },
+    temp:{
+      marginTop: 40,
+      fontSize : 170,
+    },
+    description:{
+      marginTop: 10,
+      fontSize :60 
     }
   })
 //view = flexbox
